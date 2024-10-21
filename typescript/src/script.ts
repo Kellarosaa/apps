af954a925dadb974377be371805eda26659d7e86
Kellarosaa <146978293+Kellarosaa@users.noreply.github.com>
2024-10-21 09:25:39 -0500

    Create script.ts

---
+++ typescript/src/script.ts
@@ -0,0 +1,7 @@
+// Note: please restart the page if syntax highlighting works bad.
+let el = document.querySelector('#header')
+
+let msg: string = 'Hi friend, try edit me!'
+el.innerHTML = msg
+
+console.log('it shows results as you type')
